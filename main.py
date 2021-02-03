f043fa20e6d370c1176154867b348332e263920c
AlphaDown <stoda2005@gmail.com>
2021-02-03 20:14:43 +0000

    added more knights

--- main.py
+++ main.py
@@ -1,5 +1,11 @@
 import pygame 
 from pygame.locals import *
+from sprite_loader import *
+from cat import *
+
+cat_images = []
+
+cats = []
 
 pygame.init()
 
@@ -11,17 +17,40 @@ screen = pygame.display.set_mode(size)
 
 clock = pygame.time.Clock()
 
-color = (0,0,0)
+color = (2, 194, 72)
+
+def get_images():
+  cat_sheet = SpriteSheet('knightRunning.png') 
+  #get individual images
+  
+  for i in range(1):
+    for j in range(6):
+      cat_images.append(cat_sheet.get_image(j*70,i*100,70,100))
+      #cat_images[-1] = pygame.transform.smoothscale(cat_images[-1],(616,100))
 
 #444 x 72
 def main():
+  get_images()
+
+  for i in range(5):
+    cat = Cat((-90, random.randint(50,height-50)),cat_images)
+    cats.append(cat)
+  #cat1 = Cat((-90, random.randint(50,height-50)),cat_images)
+
   while True:
     clock.tick(60)
     for event in pygame.event.get():
       if event.type == QUIT:
         sys.exit()
-      screen.fill(color)
-      pygame.display.flip()
+    
+
+    screen.fill(color)
+
+    for cat in cats:
+      cat.update()
+      cat.draw(screen)
+      
+    pygame.display.flip()
 
 if __name__ == "__main__":
   main()
--- sprite_loader.py
+++ sprite_loader.py
@@ -1,11 +1,11 @@
-import pygame
+import pygame 
 
 class SpriteSheet:
   def __init__(self,img_path):
     self.sprite_sheet = pygame.image.load(img_path)
-
   def get_image(self,x,y,width,height):
-    image = pygame.Surface([width,height])
-    image.blit(self.sprite_sheet,(0,0))
-    image.set_colorkey(image.get_at((0,0)))
+    #self.sprite_sheet.convert_alpha()
+    image = pygame.Surface([width,height], pygame.SRCALPHA)
+    image.blit(self.sprite_sheet, (0,0),(x,y,width,height))
+    image.set_colorkey((120, 92, 67))
     return image
